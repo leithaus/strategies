473e141345397e3d1d6e3fe04c6d650300511a5c
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2015-08-20 11:49:24 -0700

    Added validation utilities

--- src/main/scala/com/synereo/casper/validator.scala
+++ src/main/scala/com/synereo/casper/validator.scala
@@ -8,6 +8,42 @@
 
 package com.synereo.casper
 
+trait ValidityCheckUtilsT[Address,Data,Hash,Signature,AppState,Timer] {
+  type StateFn[State] = ( State, TxnT[Address,Data,Hash,Signature] ) => State
+  def hash[State] : State => Hash
+  def validTxn[State](
+    state : State,
+    transitionFn : StateFn[State],
+    txn : TxnT[Address,Data,Hash,Signature]
+  ) : ( Boolean, State ) = {
+    val nxtState = transitionFn( state, txn )
+    ( 
+      ( txn.prev == hash( state ) ) && ( txn.post == hash( nxtState ) ),
+      nxtState 
+    )    
+  }
+  def loopTest[State](
+    testFn : ( State, StateFn[State], TxnT[Address,Data,Hash,Signature] ) => ( Boolean, State ),
+    acc : ( Boolean, State ),
+    stateFn : StateFn[State],
+    candidates : Seq[TxnT[Address,Data,Hash,Signature]]
+  ) : ( Boolean, State ) = {
+    candidates match {
+      case Nil => acc
+      case c :: rest => {
+        val ( cond, state ) = acc
+        val nxtAcc = testFn( state, stateFn, c )
+        if ( nxtAcc._1 ) {
+          loopTest( testFn, nxtAcc, stateFn, rest )
+        }
+        else {
+          nxtAcc
+        }
+      }
+    }                                        
+  }
+}
+
 trait ValidatorT[Address,Data,Hash,Signature,AppState,Timer] {
   def initConsensusManagerState : ConsensusManagerStateT[Address,Data,Hash,Signature]
   def initAppState : AppState
